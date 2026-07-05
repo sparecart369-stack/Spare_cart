fa0554bdb320c1cd3afbd1bbcc01728dfb9e3bba
salahvk <salahkoyilandy@gmail.com>
2026-07-05 12:03:26 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker_android/image_picker_android.dart';
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
 import 'package:spare_kart/app.dart';
 import 'package:spare_kart/core/config/env_config.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
+void _configureImagePicker() {
+  final implementation = ImagePickerPlatform.instance;
+  if (implementation is ImagePickerAndroid) {
+    implementation.useAndroidPhotoPicker = true;
+  }
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  _configureImagePicker();
 
   await Supabase.initialize(
     url: EnvConfig.supabaseUrl,
